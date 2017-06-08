0ee731083c36fb9f52c988e42d401340c9a42d0d
Jonathan Tam <Jonathant@supermicro.com>
2017-06-08 15:53:03 -0700

    add batch number variable

--- src/jsonData.java
+++ src/jsonData.java
@@ -1,13 +1,17 @@
 public class jsonData {
+	private int id;
 	private String name;
 	private String part;
 	private String material;
+	private String batch;
 	private int qty;
 
-	public jsonData(String name, String part, String material, int qty) {
+	public jsonData(int id, String name, String part, String material, String batch, int qty) {
+		this.id = id;
 		this.name = name;
 		this.part = part;
 		this.material = material;
+		this.batch = batch;
 		this.qty = qty;
 	}
 }
